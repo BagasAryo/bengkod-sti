e515e279d439bb278cbd8ae5f807660f745d552a
Bagas Aryo <bagas.priyatama04@gmail.com>
2025-04-03 09:53:14 +0700

    menambahkan template adminlte, membaginya ke dalam 4 bagian, app, header, sidebar, footer, menambahkan routing layout

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function(){
+    return view('layout.app');
+});
+
+// Dokter
+Route::get('/dokter/dashboard', function(){
+    return view('dokter.dashboard');
+})->name('dokter.dashboard');
+
+// Pasien
+Route::get('/pasien/dashboard', function(){
+    return view('pasien.dashboard');
+})->name('pasien.dashboard');
